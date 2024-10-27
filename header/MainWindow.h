98fa2a49524368b7427421360eb226ecc550b480
Lei0x1 <lei0x1@yahoo.com>
2024-10-27 06:11:52 -0700

    Added app list and JSON support to MainWindow.

--- header/MainWindow.h
+++ header/MainWindow.h
@@ -2,24 +2,50 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QListWidget>
+#include <QListWidgetItem>
+#include <QDialog>
+#include <QString>
+#include <QList>
+#include <QTextEdit>
+#include <QMap>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+struct AppInfo {
+    QString defaultUrl;
+    QString outputFileName;
+};
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
-
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+protected:
+    void showEvent(QShowEvent *event) override; // Declare the showEvent method
 
 private slots:
-    void onOkClicked();
-    void onCancelClicked();
+    void onAppClicked(QListWidgetItem *item);    // Handle app selection
+    void onHomeClicked();                        // Handle home button click
+    void onChangelogClicked();                   // Handle changelog button click
+    void onAuthorClicked();                      // Handle author button click
+    void onCancelButtonClicked();                // Handle cancel button click
+
+private:
+    void loadConfig();
+    QString getDefaultDownloadURL(const QString &appName);
+    QString getDefaultOutputFileName(const QString &appName);
+
+    QListWidget *appListWidget;
+    QDialog *downloadDialog;
+    QString currentDownloadURL;
+    QString appName;
+    QTextEdit *changelogDisplay;
+    QList<QListWidgetItem*> initialAppList;
+
+    // Support Json
+    QMap<QString, AppInfo> appData;
+
+    bool inHomeView = true;  // Used in onHomeClicked();
 };
 
 #endif // MAINWINDOW_H
